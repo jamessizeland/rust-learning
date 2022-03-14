be1b9b5fa33f57b9856ea30776b95e3e6a9c8ef8
James Sizeland <jamessizeland@gmail.com>
2022-03-14 23:09:05 +0000

    add starter project 'variables'

---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/james/Repos/rust/rust-learning/variables/target/debug/variables: /home/james/Repos/rust/rust-learning/variables/src/main.rs
---
+++ variables/target/rls/debug/deps/variables-d72ee6f5a3b1a5e7.d
@@ -0,0 +1,5 @@
+/home/james/Repos/rust/rust-learning/variables/target/rls/debug/deps/variables-d72ee6f5a3b1a5e7.rmeta: src/main.rs
+
+/home/james/Repos/rust/rust-learning/variables/target/rls/debug/deps/variables-d72ee6f5a3b1a5e7.d: src/main.rs
+
+src/main.rs:
